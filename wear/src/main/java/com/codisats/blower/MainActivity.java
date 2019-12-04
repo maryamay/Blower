963138299f67cf4aafd785889cddfc976e01712f
Alhassan Atama <komputarist@gmail.com>
2019-12-04 15:13:16 +0100

    Created UI for Mainactivity and Added Location feature

--- wear/src/main/java/com/codisats/blower/MainActivity.java
+++ wear/src/main/java/com/codisats/blower/MainActivity.java
@@ -1,91 +1,419 @@
 package com.codisats.blower;
 
-import android.content.Intent;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
+import android.os.Handler;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.wearable.Wearable;
 
-public class MainActivity extends WearableActivity {
+import java.util.Calendar;
 
-  ListView listView;
+import androidx.core.app.ActivityCompat;
+import androidx.wear.ambient.AmbientModeSupport;
+
+
+public class MainActivity extends WearableActivity implements AmbientModeSupport.AmbientCallbackProvider,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        LocationListener {
+
+    private static final String TAG = "WearableActivity";
+
+    private static final long INDICATOR_DOT_FADE_AWAY_MS = 500L;
+
+    // Request codes for changing speed limit and location permissions.
+    private static final int REQUEST_PICK_SPEED_LIMIT = 0;
+
+    // Id to identify Location permission request.
+    private static final int REQUEST_GPS_PERMISSION = 1;
+
+    // Shared Preferences for saving speed limit and location permission between app launches.
+    private static final String PREFS_SPEED_LIMIT_KEY = "SpeedLimit";
+
+    private Calendar mCalendar;
+
+    private TextView mSpeedTextView;
+    private ImageView mGpsPermissionImageView;
+    private Button mReport;
+    private TextView mGpsIssueTextView;
+    private View mBlinkingGpsStatusDotView;
+    private Button mSent;
+
+    private String mGpsPermissionNeededMessage;
+    private String mAcquiringGpsMessage;
+
+
+    private boolean mGpsPermissionApproved;
+
+    private boolean mWaitingForGpsSignal;
+
+    /**
+     * Ambient mode controller attached to this display. Used by the Activity to see if it is in
+     * ambient mode.
+     */
+    private AmbientModeSupport.AmbientController mAmbientController;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private Handler mHandler = new Handler();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
+
         setContentView(R.layout.activity_main);
 
-        listView = (ListPView) findViewById(R.id.list_view);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(position == 0){
-                    displaySpeechScreen();
-                }else{
-                    Message message = (Message) parent.getItemAtPosition(position);
+        // Enables Ambient mode.
+        mAmbientController = AmbientModeSupport.attach(this);
 
-                    Intent intent = new Intent(getApplicationContext(), DeleteActivity.class);
-                    intent.putExtra("id", message.getId());
+        mCalendar = Calendar.getInstance();
 
-                    startActivity(intent);
-                }
-            }
-        });
+        // Enables app to handle 23+ (M+) style permissions.
+        mGpsPermissionApproved =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED;
 
-        updateUI();
+        mGpsPermissionNeededMessage = getString(R.string.permission_rationale);
+        mAcquiringGpsMessage = getString(R.string.acquiring_gps);
+
+        /*
+         * If this hardware doesn't support GPS, we warn the user. Note that when such device is
+         * connected to a phone with GPS capabilities, the framework automatically routes the
+         * location requests from the phone. However, if the phone becomes disconnected and the
+         * wearable doesn't support GPS, no location is recorded until the phone is reconnected.
+         */
+        if (!hasGps()) {
+            Log.w(TAG, "This hardware doesn't have GPS, so we warn user.");
+            new AlertDialog.Builder(this)
+                    .setMessage(getString(R.string.gps_not_available))
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setCancelable(false)
+                    .create()
+                    .show();
+        }
+
+        setupViews();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+
+        // Enables Always-on
+        setAmbientEnabled();
     }
 
     @Override
-    public  void onResume() {
+    protected void onPause() {
+        super.onPause();
+        if ((mGoogleApiClient != null) && (mGoogleApiClient.isConnected()) &&
+                (mGoogleApiClient.isConnecting())) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+            mGoogleApiClient.disconnect();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
         super.onResume();
-
-        updateUI();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
 
-    private void updateUI() {
-        ArrayList<Message> messages = Helper.getAllMessages(this);
 
-        messages.add(0, new Message(" ",""));
+    private void setupViews() {
+        mReport = findViewById(R.id.report);
+        mReport = findViewById(R.id.report);
 
-        listView.setAdapter(new ListViewAdapter(this, 0, messages));
+        mGpsPermissionImageView = findViewById(R.id.gps_permission);
+        mGpsIssueTextView = findViewById(R.id.gps_issue_text);
+        mBlinkingGpsStatusDotView = findViewById(R.id.dot);
+
+        updateActivityViewsBasedOnLocationPermissions();
+    }
+
+
+    public void onGpsPermissionClick(View view) {
+
+        if (!mGpsPermissionApproved) {
+
+            Log.i(TAG, "Location permission has NOT been granted. Requesting permission.");
+
+            // On 23+ (M+) devices, GPS permission not granted. Request permission.
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_GPS_PERMISSION);
+        }
+    }
+
+    /**
+     * Adjusts the visibility of views based on location permissions.
+     */
+    private void updateActivityViewsBasedOnLocationPermissions() {
+
+        /*
+         * If the user has approved location but we don't have a signal yet, we let the user know
+         * we are waiting on the GPS signal (this sometimes takes a little while). Otherwise, the
+         * user might think something is wrong.
+         */
+        if (mGpsPermissionApproved && mWaitingForGpsSignal) {
+
+            // We are getting a GPS signal w/ user permission.
+            mGpsIssueTextView.setText(mAcquiringGpsMessage);
+            mGpsIssueTextView.setVisibility(View.VISIBLE);
+            mGpsPermissionImageView.setImageResource(R.drawable.ic_gps_saving_grey600_96dp);
+
+
+        } else if (mGpsPermissionApproved) {
+
+            mGpsIssueTextView.setVisibility(View.GONE);
+
+            mGpsPermissionImageView.setImageResource(R.drawable.ic_gps_saving_grey600_96dp);
+
+        } else {
+
+            // User needs to enable location for the app to work.
+            mGpsIssueTextView.setVisibility(View.VISIBLE);
+            mGpsIssueTextView.setText(mGpsPermissionNeededMessage);
+            mGpsPermissionImageView.setImageResource(R.drawable.ic_gps_not_saving_grey600_96dp);
+
+            mSpeedTextView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        Log.d(TAG, "onConnected()");
+        requestLocation();
 
 
     }
 
-    public void displaySpeechScreen() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What is the title for the message?");
+    private void requestLocation() {
+        Log.d(TAG, "requestLocation()");
 
-                //Starts the activity, intent will be populated with speech data text
-                startActivityForResult(intent, 1001);
+        /*
+         * mGpsPermissionApproved covers 23+ (M+) style permissions. If that is already approved or
+         * the device is pre-23, the app uses mSaveGpsLocation to save the user's location
+         * preference.
+         */
+        if (mGpsPermissionApproved) {
 
+            LocationRequest locationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(UPDATE_INTERVAL_MS)
+                    .setFastestInterval(FASTEST_INTERVAL_MS);
+
+            LocationServices.FusedLocationApi
+                    .requestLocationUpdates(mGoogleApiClient, locationRequest, this)
+                    .setResultCallback(new ResultCallback<Status>() {
+
+                        @Override
+                        public void onResult(Status status) {
+                            if (status.getStatus().isSuccess()) {
+                                if (Log.isLoggable(TAG, Log.DEBUG)) {
+                                    Log.d(TAG, "Successfully requested location updates");
+                                }
+                            } else {
+                                Log.e(TAG,
+                                        "Failed in requesting location updates, "
+                                                + "status code: "
+                                                + status.getStatusCode() + ", message: " + status
+                                                .getStatusMessage());
+                            }
+                        }
+                    });
+        }
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == 1001 && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended(): connection to location client suspended");
 
-            String msg = results.get(0);
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+    }
 
-            Message message = new Message(null, msg);
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed(): " + connectionResult.getErrorMessage());
+    }
 
-            Helper.saveMessage(message, this);
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged() : " + location);
 
-            Helper.displayConfirmation("Message Saved!", this);
 
-            updateUI();
+        if (mWaitingForGpsSignal) {
+            mWaitingForGpsSignal = false;
+            updateActivityViewsBasedOnLocationPermissions();
+        }
+
+        addLocationEntry(location.getLatitude(), location.getLongitude());
+    }
+
+    /*
+     * Adds a data item to the data Layer storage.
+     */
+    private void addLocationEntry(double latitude, double longitude) {
+        if (!mGpsPermissionApproved || !mGoogleApiClient.isConnected()) {
+            return;
+        }
+        mCalendar.setTimeInMillis(System.currentTimeMillis());
+        LocationEntry entry = new LocationEntry(mCalendar, latitude, longitude);
+        String path = Constants.PATH + "/" + mCalendar.getTimeInMillis();
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(path);
+        putDataMapRequest.getDataMap().putDouble(Constants.KEY_LATITUDE, entry.latitude);
+        putDataMapRequest.getDataMap().putDouble(Constants.KEY_LONGITUDE, entry.longitude);
+        putDataMapRequest.getDataMap()
+                .putLong(Constants.KEY_TIME, entry.calendar.getTimeInMillis());
+        PutDataRequest request = putDataMapRequest.asPutDataRequest();
+        request.setUrgent();
+        Wearable.DataApi.putDataItem(mGoogleApiClient, request)
+                .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+                    @Override
+                    public void onResult(DataApi.DataItemResult dataItemResult) {
+                        if (!dataItemResult.getStatus().isSuccess()) {
+                            Log.e(TAG, "AddPoint:onClick(): Failed to set the data, "
+                                    + "status: " + dataItemResult.getStatus()
+                                    .getStatusCode());
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Handles user choices for both speed limit and location permissions (GPS tracking).
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_PICK_SPEED_LIMIT) {
+            if (resultCode == RESULT_OK) {
+                // The user updated the speed limit.
+                int newSpeedLimit =
+                        data.getIntExtra(SpeedPickerActivity.EXTRA_NEW_SPEED_LIMIT, mSpeedLimit);
+
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(WearableMainActivity.PREFS_SPEED_LIMIT_KEY, newSpeedLimit);
+                editor.apply();
+
+                mSpeedLimit = newSpeedLimit;
+
+                updateSpeedInViews();
+            }
+        }
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        Log.d(TAG, "onRequestPermissionsResult(): " + permissions);
+
+
+        if (requestCode == REQUEST_GPS_PERMISSION) {
+            Log.i(TAG, "Received response for GPS permission request.");
+
+            if ((grantResults.length == 1)
+                    && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Log.i(TAG, "GPS permission granted.");
+                mGpsPermissionApproved = true;
+
+                if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+                    requestLocation();
+                }
+
+            } else {
+                Log.i(TAG, "GPS permission NOT granted.");
+                mGpsPermissionApproved = false;
+            }
+
+            updateActivityViewsBasedOnLocationPermissions();
 
         }
     }
 
+    /**
+     * Returns {@code true} if this device has the GPS capabilities.
+     */
+    private boolean hasGps() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+    }
+
+    @Override
+    public AmbientModeSupport.AmbientCallback getAmbientCallback() {
+        return new MyAmbientCallback();
+    }
+
+    private class MyAmbientCallback extends AmbientModeSupport.AmbientCallback {
+        /**
+         * Prepares the UI for ambient mode.
+         */
+        @Override
+        public void onEnterAmbient(Bundle ambientDetails) {
+            super.onEnterAmbient(ambientDetails);
+
+            Log.d(TAG, "onEnterAmbient() " + ambientDetails);
+
+            // Changes views to grey scale.
+            mSpeedTextView.setTextColor(
+                    ContextCompat.getColor(getApplicationContext(), R.color.white));
+        }
+
+        /**
+         * Restores the UI to active (non-ambient) mode.
+         */
+        @Override
+        public void onExitAmbient() {
+            super.onExitAmbient();
+
+            Log.d(TAG, "onExitAmbient()");
+
+            // Changes views to color.
+            mSpeedTextView.setTextColor(
+                    ContextCompat.getColor(getApplicationContext(), R.color.green));
+        }
+    }
 
 }
